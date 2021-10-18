79762c652a8ca006801dc91918a2d5f58fbdb68a
Malcolm Sinclair <sinclair.m.x@gmail.com>
2021-10-18 15:28:34 -0400

    add variables for meme form and meme list

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //Need to wait for page to load
-// 
-
 document.addEventListener('DOMContentLoaded', function(){
+//Need to be able to refer to my meme form and the meme list
+    let memeForm = document.getElementById('meme-form');
+    let memeList = document.getElementById('meme-list');
 
-    
+
+
 })
