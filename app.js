01e872dbd76c5511d9e26688ef710581279ff567
Malcolm Sinclair <sinclair.m.x@gmail.com>
2021-10-18 18:02:04 -0400

    add event listener for form submission, bugfix on innerText for top and bottom text

--- app.js
+++ app.js
@@ -4,33 +4,43 @@ document.addEventListener('DOMContentLoaded', function(){
     let memeForm = document.getElementById('meme-form');
     let memeList = document.getElementById('meme-list');
 
+    //Need to know when a meme is being created
+    memeForm.addEventListener("submit", function(event) {
+        event.preventDefault();
     //Each meme that is created has to be a list item for the memeList
     let createdMeme = document.createElement('li');
     createdMeme.classList.add('created-meme');
 
     //Need to create element for the image that is uploaded
-    let memeImgLocation = document.getElementById('meme-image').value;
-    let src = memeImgLocation;
+    let linkLocation = document.getElementById('meme-image').value;
+    src = linkLocation;
     let memeImg = document.createElement('img');
-    memeImg.src = memeImgLocation;
+    memeImg.src = src;
 
     //Need to create element to overlay text at the top
     let topTextContainer = document.createElement('div');
-    topTextContainer.innerText = document.getElementById('top-text');
+    topTextContainer.innerText = document.getElementById("tp-text").value;
 
     //Need to create element to overlay text at the bottom
     let bottomTextContainer = document.createElement('div');
-    bottomTextContainer.innerText = document.getElementById('bottom-text');
+    bottomTextContainer.innerText = document.getElementById("bt-text").value;
 
     //Need to add close button to the created meme
     let deleteMeme = document.createElement('div');
     deleteMeme.innerText = "Delete Meme";
 
-    //Need to add the image, top text, bottom text, and close button to the created meme
-
     //Need to add the created meme to my meme list
+    //Need to add the image, top text, bottom text, and close button to the created meme
+    memeList.appendChild(createdMeme);
+    createdMeme.appendChild(memeImg);
+    createdMeme.appendChild(topTextContainer);
+    createdMeme.appendChild(bottomTextContainer);
+    createdMeme.appendChild(deleteMeme);
 
     //After meme is created and added I should clear inputs from my form
+    memeForm.reset();
+
+    })
 
 })
 
